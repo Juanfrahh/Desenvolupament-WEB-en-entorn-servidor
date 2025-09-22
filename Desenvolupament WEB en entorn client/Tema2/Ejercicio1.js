11b14eb1f0e3d88de77a75888f3e562f85e99e22
Juanfrahh <carlostortosa2005@gmail.com>
2025-09-22 15:53:11 +0200

    Auto-commit

--- Desenvolupament WEB en entorn client/Tema2/Ejercicio1.js
+++ Desenvolupament WEB en entorn client/Tema2/Ejercicio1.js
@@ -6,7 +6,7 @@ const comparacion = function(){
     if (text1 === texto2){
         alert("El texto " + texto1 + " y " + texto2 + "son iguales");
     }else{
-        alert("El texto " + texto1 + " y " + texto2 + "no son iguales")
+        alert("El texto " + texto1 + " y " + texto2 + "no son iguales");
     }
 } 
 }while(confirm(("Quieres volver a jugar?")));
