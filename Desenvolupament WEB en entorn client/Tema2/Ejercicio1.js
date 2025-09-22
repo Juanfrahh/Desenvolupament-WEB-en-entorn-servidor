ea44448288e8282954f58a5656abc4221e55c6df
Juanfrahh <carlostortosa2005@gmail.com>
2025-09-22 16:21:20 +0200

    Auto-commit

--- Desenvolupament WEB en entorn client/Tema2/Ejercicio1.js
+++ Desenvolupament WEB en entorn client/Tema2/Ejercicio1.js
@@ -2,7 +2,7 @@ do{
 const text1 = prompt ("Dame la primera cadena de texto");
 const texto2 = prompt ("Dame la segunda cadena de texto");
 
-window.alert(texto1);
+window.alert(""texto1);
 window.alert(texto2);
     if (text1.trim.localcompare(texto2.trim)){
         window.alert("El texto " + texto1 + " y " + texto2 + "son iguales");
