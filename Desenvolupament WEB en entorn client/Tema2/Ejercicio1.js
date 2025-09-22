3440e623e3adc5efb34b514dd9cd647d08543ac8
Juanfrahh <carlostortosa2005@gmail.com>
2025-09-22 15:46:36 +0200

    Auto-commit

--- Desenvolupament WEB en entorn client/Tema2/Ejercicio1.js
+++ Desenvolupament WEB en entorn client/Tema2/Ejercicio1.js
@@ -1,4 +1,6 @@
 let text1 = prompt ("Dame la primera cadena de texto");
 let texto2 = prompt ("Dame la segunda cadena de texto");
 
-const comparacion = function() 
+const comparacion = function(){
+    
+} 
