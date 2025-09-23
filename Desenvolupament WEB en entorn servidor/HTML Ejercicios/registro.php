8a4d40f29470e4958d90b2a2a475224ccaec124c
Juanfrahh <carlostortosa2005@gmail.com>
2025-09-23 17:11:16 +0200

    Auto-commit

--- Desenvolupament WEB en entorn servidor/HTML Ejercicios/registro.php
+++ Desenvolupament WEB en entorn servidor/HTML Ejercicios/registro.php
@@ -13,7 +13,7 @@
     <label for="nombre">Nombre:</label>
     <input type="text" id="nombre" name="nombre" required>
     <br>
-
+    
     <label for="email">Email:</label>
     <input type="email" id="email" name="email" required>
     <br>
