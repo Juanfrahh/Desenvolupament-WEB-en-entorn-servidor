85cd0cc9d1df21f72efb348cbc1c20e54cfd9b14
Juanfrahh <carlostortosa2005@gmail.com>
2025-09-23 17:00:07 +0200

    Auto-commit

--- Desenvolupament WEB en entorn servidor/HTML Ejercicios/footer.inc.php
+++ Desenvolupament WEB en entorn servidor/HTML Ejercicios/footer.inc.php
@@ -9,7 +9,7 @@
         $mesNumero = date("n");
         $anio = date("Y");
         $diaSemanaNumero = date("w");
-
+ 
         $fechas = [
             "dias" => ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"],
             "meses" => ["", "enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"]
