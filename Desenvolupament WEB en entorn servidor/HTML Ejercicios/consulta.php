d9d57ccb6fffd2be0d465a2302f330baca296120
Juanfrahh <carlostortosa2005@gmail.com>
2025-09-23 15:29:23 +0200

    Auto-commit

--- Desenvolupament WEB en entorn servidor/HTML Ejercicios/consulta.php
+++ Desenvolupament WEB en entorn servidor/HTML Ejercicios/consulta.php
@@ -16,7 +16,7 @@
                 <th>Valor</th>
             </tr>
         <?php
-            foreach ($_)
+            foreach ($_POST as )
         ?>
     </main>
     <?php include("footer.inc.php") ?>
