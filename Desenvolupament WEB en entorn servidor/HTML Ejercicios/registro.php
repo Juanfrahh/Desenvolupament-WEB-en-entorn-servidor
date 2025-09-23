1dbbd1fa82c4d03ca4c6c4c8b08d35b36c58f45c
Juanfrahh <carlostortosa2005@gmail.com>
2025-09-23 16:28:13 +0200

    Auto-commit

--- Desenvolupament WEB en entorn servidor/HTML Ejercicios/registro.php
+++ Desenvolupament WEB en entorn servidor/HTML Ejercicios/registro.php
@@ -7,6 +7,27 @@
 </head>
 <body>
     <header><?php include("cabecera.inc.php") ?></header>
-    
+    <main>
+        <h2>Datos recibidos: </h2>
+        <br>
+        <table border="1">
+            <tr>
+                <th>Campo</th>
+                <th>Valor</th>
+            </tr>
+        <?php
+            foreach ($_POST as $campo => $valor) {
+                
+                echo "<tr>";
+                echo "<td>$campo</td>";
+
+                if (is_array($valor)) {
+                    echo "<td>" . implode(", ", $valor) . "</td>";
+                } else {
+                    echo "<td>$valor</td>";
+                }
+                echo "</tr>";
+            }
+        ?>
 </body>
 </html>
