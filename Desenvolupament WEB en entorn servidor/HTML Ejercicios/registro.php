01238084aae40de9bbd07f6ea7562de696d33f6f
Juanfrahh <carlostortosa2005@gmail.com>
2025-09-23 16:30:05 +0200

    Auto-commit

--- Desenvolupament WEB en entorn servidor/HTML Ejercicios/registro.php
+++ Desenvolupament WEB en entorn servidor/HTML Ejercicios/registro.php
@@ -6,7 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <?php include("cabecera.inc.php") ?></header>
+    <?php include("cabecera.inc.php") ?>
     <main>
         
     <footer><?php include("footer.inc.php") ?></footer>
