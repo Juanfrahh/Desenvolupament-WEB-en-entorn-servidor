c821686a85491b625e86b29a067fd17fef4185a2
Juanfrahh <carlostortosa2005@gmail.com>
2025-09-24 16:21:40 +0200

    Auto-commit

--- Desenvolupament WEB en entorn client/Tema1/ejercicio3.js
+++ Desenvolupament WEB en entorn client/Tema1/ejercicio3.js
@@ -5,7 +5,7 @@ num2 = parseInt(prompt("Introduce el numero de veces que quieras que se duplique
 
 document.write("El numero es "+num1 + " y se hallará " + num2 + " veces" );
 
-if (
+if ()
 document.write("<table border='1'>");
 for ( let i = 0; i < num2; i++) {
     document.writeln("<br>"+ (num1 = num1 * 2));
