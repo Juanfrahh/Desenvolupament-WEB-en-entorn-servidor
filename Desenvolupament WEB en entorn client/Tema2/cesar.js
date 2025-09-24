04fced325c4a8a84ed37f5fd36e90d7838e169ad
Juanfrahh <carlostortosa2005@gmail.com>
2025-09-24 15:42:20 +0200

    Auto-commit

--- Desenvolupament WEB en entorn client/Tema2/cesar.js
+++ Desenvolupament WEB en entorn client/Tema2/cesar.js
@@ -1,6 +1,6 @@
 let texto = prompt("Introduce un texto para cifrarlo con el cifrado César:");
 let desplazamiento;
-desplazamiento = parseInt(prompt("Introduce el numero de desplazamientos"));
+desplazamiento.toFixed(0) = parseInt(prompt("Introduce el numero de desplazamientos"));
 
 do{
 
