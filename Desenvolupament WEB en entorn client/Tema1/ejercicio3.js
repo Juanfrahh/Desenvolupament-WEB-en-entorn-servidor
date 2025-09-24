1eca00b22be1315724084906b339ca65579dee5b
Juanfrahh <carlostortosa2005@gmail.com>
2025-09-24 16:22:01 +0200

    Auto-commit

--- Desenvolupament WEB en entorn client/Tema1/ejercicio3.js
+++ Desenvolupament WEB en entorn client/Tema1/ejercicio3.js
@@ -6,6 +6,7 @@ num2 = parseInt(prompt("Introduce el numero de veces que quieras que se duplique
 document.write("El numero es "+num1 + " y se hallará " + num2 + " veces" );
 
 if (num1 === "" || isNaN(num1) || num2 === "" || isNaN(num2) || num2 < 0) {
+    alert("El dato no es válido o has cancelado");
 
 document.write("<table border='1'>");
 for ( let i = 0; i < num2; i++) {
