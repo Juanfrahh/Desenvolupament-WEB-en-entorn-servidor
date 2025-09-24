9a8f8a36abd6d2f3087ee6b41222d519862872c4
Juanfrahh <carlostortosa2005@gmail.com>
2025-09-24 16:22:36 +0200

    Auto-commit

--- Desenvolupament WEB en entorn client/Tema1/ejercicio3.js
+++ Desenvolupament WEB en entorn client/Tema1/ejercicio3.js
@@ -11,8 +11,5 @@ if (num1 === "" || isNaN(num1) || num2 === "" || isNaN(num2) || num2 < 0) {
     break;
 }
 
-document.write("<table border='1'>");
-for ( let i = 0; i < num2; i++) {
-    document.writeln("<br>"+ (num1 = num1 * 2));
-}
+
 }while(true);
