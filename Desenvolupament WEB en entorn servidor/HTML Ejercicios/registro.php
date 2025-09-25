e1520bc95c4929fb8392d9ec1869f81fe1a6599b
Juanfrahh <carlostortosa2005@gmail.com>
2025-09-25 18:23:20 +0200

    Auto-commit

--- Desenvolupament WEB en entorn servidor/HTML Ejercicios/registro.php
+++ Desenvolupament WEB en entorn servidor/HTML Ejercicios/registro.php
@@ -75,7 +75,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <title>Registro</title>
 </head>
 <body>
-<?php include("cabecera.inc.php"); ?>
+
 <main>
 <?php if ($_SERVER["REQUEST_METHOD"] == "POST" && empty($errores)): ?>
     <h2>Registro completado</h2>
