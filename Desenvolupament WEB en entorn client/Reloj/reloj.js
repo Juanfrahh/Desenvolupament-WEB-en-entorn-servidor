e542083ad2fe4f02d09bcb098aca19dd5e6a8818
Juanfrahh <carlostortosa2005@gmail.com>
2025-10-01 17:12:03 +0200

    Auto-commit

--- Desenvolupament WEB en entorn client/Reloj/reloj.js
+++ Desenvolupament WEB en entorn client/Reloj/reloj.js
@@ -6,7 +6,7 @@ function mostrarReloj(){
     let horas = fecha.getHours();
     let minutos = fecha.getMinutes();
     let segundos = fecha.getSeconds();
-    let 
+    let a
     let horaActual = `${horas}:${minutos}:${segundos}`;
     reloj.textContent = horaActual;
 
