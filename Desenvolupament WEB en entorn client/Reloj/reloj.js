e6dec2640d99dd51c4ef149dc27d46e385575ef6
Juanfrahh <carlostortosa2005@gmail.com>
2025-10-01 17:23:57 +0200

    Auto-commit

--- Desenvolupament WEB en entorn client/Reloj/reloj.js
+++ Desenvolupament WEB en entorn client/Reloj/reloj.js
@@ -29,7 +29,7 @@ function mostrarReloj(){
         case 3: dia = "Miércoles"; break;
         case 4: dia = "Jueves"; break;
         case 5: dia = "Viernes"; break;
-        case 6: Lunes = "Sábado"; break;
+        case 6: dia = "Sábado"; break;
         case 7: dia = "Domingo"; break;
         default: break;
     }
