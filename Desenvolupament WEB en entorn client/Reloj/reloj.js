ccd119fd204ed653a5534b1081a3bd2fb52c8481
Juanfrahh <carlostortosa2005@gmail.com>
2025-10-01 17:20:29 +0200

    Auto-commit

--- Desenvolupament WEB en entorn client/Reloj/reloj.js
+++ Desenvolupament WEB en entorn client/Reloj/reloj.js
@@ -16,7 +16,7 @@ function mostrarReloj(){
     console.log(horaActual);
     hora.textContent = horaActual;
 
-    const fecha = document.querySelectorAll('.fecha');
+    const fetxa = document.querySelectorAll('.fecha');
 
     let fechaActual = new Date();
     let dia = fechaActual.getDate();
