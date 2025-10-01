5967b6c7a7c582b41e5ef7452c9875669665cc88
Juanfrahh <carlostortosa2005@gmail.com>
2025-10-01 17:26:22 +0200

    Auto-commit

--- Desenvolupament WEB en entorn client/Reloj/reloj.js
+++ Desenvolupament WEB en entorn client/Reloj/reloj.js
@@ -23,7 +23,7 @@ function mostrarReloj(){
     let mes = fechaActual.getMonth() + 1;
     let ano = fechaActual.getFullYear();
 
-    diaSemana
+    diaSemana = fechaActual.getDay();
 
     switch (mes) {
         case 1: mes = "Ene"; break;
