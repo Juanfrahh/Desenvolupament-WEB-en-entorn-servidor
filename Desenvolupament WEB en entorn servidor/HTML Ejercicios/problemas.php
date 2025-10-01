2374a9b62c0d693557c0055c6f2027cf8660dbe2
Juanfrahh <carlostortosa2005@gmail.com>
2025-10-01 18:36:34 +0200

    Auto-commit

--- Desenvolupament WEB en entorn servidor/HTML Ejercicios/problemas.php
+++ Desenvolupament WEB en entorn servidor/HTML Ejercicios/problemas.php
@@ -31,7 +31,6 @@
 <?php
 // ---------------- FUNCIONES -----------------
 
-// Sumar
 function sumar($a, $b) {
     if (!is_numeric($a) || !is_numeric($b)) {
         throw new Exception("Ambos parámetros deben ser números.");
