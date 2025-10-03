a222fa1e20ac61a0fc367eb5fbcabad7b9f0f218
Juanfrahh <carlostortosa2005@gmail.com>
2025-10-03 17:05:43 +0200

    Auto-commit

--- Desenvolupament WEB en entorn client/Reloj/reloj.js
+++ Desenvolupament WEB en entorn client/Reloj/reloj.js
@@ -51,6 +51,6 @@ function mostrarReloj(){
     let fechaCompleta = diaSemana + ", " + dia + " " + mes ;
     fecha.textContent = fechaCompleta;
 
-    document.getElementById()
+    document.getElementById("contenedor")
 }
 setInterval(mostrarReloj, 1000);
