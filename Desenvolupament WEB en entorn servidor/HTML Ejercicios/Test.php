b8db21a6fedc2b397177c6a6a62b74aeb281fc60
Juanfrahh <carlostortosa2005@gmail.com>
2025-10-07 15:13:53 +0200

    Auto-commit

---
+++ Desenvolupament WEB en entorn servidor/HTML Ejercicios/Test.php
@@ -0,0 +1,45 @@
+<?php
+echo "<h2>Prueba de clases de herencia (UD3)</h2>";
+
+// 1️⃣ Probamos la clase Soporte
+include "Soporte.php";
+
+$soporte1 = new Soporte("Tenet", 22, 3);
+echo "<strong>" . $soporte1->titulo . "</strong><br>";
+echo "Precio: " . $soporte1->getPrecio() . " euros<br>";
+echo "Precio IVA incluido: " . $soporte1->getPrecioConIVA() . " euros<br>";
+$soporte1->muestraResumen();
+
+echo "<hr>";
+
+// 2️⃣ Probamos la clase CintaVideo
+include "CintaVideo.php";
+
+$miCinta = new CintaVideo("Los cazafantasmas", 23, 3.5, 107);
+echo "<strong>" . $miCinta->titulo . "</strong><br>";
+echo "Precio: " . $miCinta->getPrecio() . " euros<br>";
+echo "Precio IVA incluido: " . $miCinta->getPrecioConIVA() . " euros<br>";
+$miCinta->muestraResumen();
+
+echo "<hr>";
+
+// 3️⃣ Probamos la clase Dvd
+include "Dvd.php";
+
+$miDvd = new Dvd("Origen", 24, 15, "es,en,fr", "16:9");
+echo "<strong>" . $miDvd->titulo . "</strong><br>";
+echo "Precio: " . $miDvd->getPrecio() . " euros<br>";
+echo "Precio IVA incluido: " . $miDvd->getPrecioConIVA() . " euros<br>";
+$miDvd->muestraResumen();
+
+echo "<hr>";
+
+// 4️⃣ Probamos la clase Juego
+include "Juego.php";
+
+$miJuego = new Juego("The Last of Us Part II", 26, 49.99, "PS4", 1, 1);
+echo "<strong>" . $miJuego->titulo . "</strong><br>";
+echo "Precio: " . $miJuego->getPrecio() . " euros<br>";
+echo "Precio IVA incluido: " . $miJuego->getPrecioConIVA() . " euros<br>";
+$miJuego->muestraResumen();
+?>
