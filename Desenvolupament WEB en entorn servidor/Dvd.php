0bba4cbaaa61f0711076342f5d631ebfc165bd22
Juanfrahh <carlostortosa2005@gmail.com>
2025-10-07 15:12:05 +0200

    Auto-commit

--- Desenvolupament WEB en entorn servidor/Dvd.php
+++ Desenvolupament WEB en entorn servidor/Dvd.php
@@ -1,19 +1,22 @@
 <?php
 require_once "Soporte.php";
 
-class CintaVideo extends Soporte {
-    private $duracion; // en minutos
+class Dvd extends Soporte {
+    private $idiomas;
+    private $formatoPantalla;
 
-    public function __construct($titulo, $numero, $precio, $duracion) {
+    public function __construct($titulo, $numero, $precio, $idiomas, $formatoPantalla) {
         parent::__construct($titulo, $numero, $precio);
-        $this->duracion = $duracion;
+        $this->idiomas = $idiomas;
+        $this->formatoPantalla = $formatoPantalla;
     }
 
     public function muestraResumen() {
-        echo "<br>Película en VHS:<br>";
+        echo "<br>Película en DVD:<br>";
         echo $this->titulo . "<br>";
         echo $this->precio . " € (IVA no incluido)<br>";
-        echo "Duración: " . $this->duracion . " minutos<br>";
+        echo "Idiomas: " . $this->idiomas . "<br>";
+        echo "Formato Pantalla: " . $this->formatoPantalla . "<br>";
     }
 }
 ?>
