75643c91fba05e69e3c2f977c4a24f74a80485e7
Juanfrahh <carlostortosa2005@gmail.com>
2025-10-07 15:11:25 +0200

    Auto-commit

---
+++ Desenvolupament WEB en entorn servidor/CintaVideo.php
@@ -0,0 +1,19 @@
+<?php
+require_once "Soporte.php";
+
+class CintaVideo extends Soporte {
+    private $duracion; // en minutos
+
+    public function __construct($titulo, $numero, $precio, $duracion) {
+        parent::__construct($titulo, $numero, $precio);
+        $this->duracion = $duracion;
+    }
+
+    public function muestraResumen() {
+        echo "<br>Película en VHS:<br>";
+        echo $this->titulo . "<br>";
+        echo $this->precio . " € (IVA no incluido)<br>";
+        echo "Duración: " . $this->duracion . " minutos<br>";
+    }
+}
+?>
