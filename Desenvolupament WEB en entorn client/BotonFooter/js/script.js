5a96e668d5a91eca015c13bec961c25e24f721bf
Juanfrahh <carlostortosa2005@gmail.com>
2025-10-08 15:31:49 +0200

    Auto-commit

---
+++ Desenvolupament WEB en entorn client/BotonFooter/js/script.js
@@ -0,0 +1,26 @@
+// Espera a que el DOM esté cargado
+document.addEventListener('DOMContentLoaded', function () {
+    const btnDescubreMas = document.getElementById('btn-descubre-mas');
+    const footer = document.querySelector('footer');
+
+    // Oculta el footer al inicio
+    if (footer) {
+        footer.style.display = 'none';
+    }
+
+    if (btnDescubreMas && footer) {
+        btnDescubreMas.addEventListener('click', function () {
+            if (footer.style.display === 'none') {
+                footer.style.display = 'block';
+                btnDescubreMas.textContent = 'XCerrar';
+                btnDescubreMas.style.backgroundColor = 'red';
+                btnDescubreMas.style.color = 'white';
+            } else {
+                footer.style.display = 'none';
+                btnDescubreMas.textContent = 'Descubre más';
+                btnDescubreMas.style.backgroundColor = '';
+                btnDescubreMas.style.color = '';
+            }
+        });
+    }
+});
