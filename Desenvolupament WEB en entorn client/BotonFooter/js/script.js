2dca1c16c9acb0f7657de46e0ab32c7ee439d010
Juanfrahh <carlostortosa2005@gmail.com>
2025-10-08 15:49:59 +0200

    Auto-commit

---
+++ Desenvolupament WEB en entorn client/BotonFooter/js/script.js
@@ -0,0 +1,15 @@
+// Suponiendo que tienes un botón con id "descubreBtn" y un footer con id "footer"
+const btn = document.getElementById('descubreBtn');
+const footer = document.getElementById('footer');
+
+btn.addEventListener('click', function() {
+    // Alternar la visibilidad del footer
+    footer.classList.toggle('abierto');
+
+    // Cambiar el texto del botón
+    if (footer.classList.contains('abierto')) {
+        btn.textContent = 'XCerrar';
+    } else {
+        btn.textContent = 'Descubre más...';
+    }
+});
