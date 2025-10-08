eaf05572744377c005a2784a5cdc473d77019aa7
Juanfrahh <carlostortosa2005@gmail.com>
2025-10-08 15:57:59 +0200

    Auto-commit

--- Desenvolupament WEB en entorn client/BotonFooter/js/script.js
+++ Desenvolupament WEB en entorn client/BotonFooter/js/script.js
@@ -4,6 +4,7 @@ const btn = document.getElementById('btn-footer');
 const footer = document.getElementById('footer');
 
 btn.addEventListener('click', function() {
+    // El footer debe de aparecer cuando le demos al boton con el id btn-footer.
     
     // Alternar la visibilidad del footer
     footer.classList.toggle('abierto');
