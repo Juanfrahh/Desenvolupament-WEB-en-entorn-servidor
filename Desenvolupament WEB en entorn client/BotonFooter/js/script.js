e5c848f514598481f5b21a9bf0670e2e8a9b93c7
Juanfrahh <carlostortosa2005@gmail.com>
2025-10-08 15:51:11 +0200

    Auto-commit

--- Desenvolupament WEB en entorn client/BotonFooter/js/script.js
+++ Desenvolupament WEB en entorn client/BotonFooter/js/script.js
@@ -1,5 +1,5 @@
 // Suponiendo que tienes un botón con id "footer" y un footer con id "footer"
-const btn = document.getElementById('btn-fotante');
+const btn = document.getElementById('btn-footante');
 const footer = document.getElementById('footer');
 
 btn.addEventListener('click', function() {
