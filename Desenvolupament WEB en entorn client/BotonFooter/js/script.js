e04e386ba637b5d39fcb0b84e3a71711760e72d1
Juanfrahh <carlostortosa2005@gmail.com>
2025-10-08 15:55:37 +0200

    Auto-commit

--- Desenvolupament WEB en entorn client/BotonFooter/js/script.js
+++ Desenvolupament WEB en entorn client/BotonFooter/js/script.js
@@ -1,4 +1,4 @@
-// El footer debe de aparecer cuando le demos al boton 
+// El footer debe de aparecer cuando le demos al boton con el id btn-footer
 const btn = document.getElementById('btn-footer');
 const footer = document.getElementById('footer');
 
