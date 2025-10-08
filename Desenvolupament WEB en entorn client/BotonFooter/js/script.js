7a92fb78f91fc12caf511f0824d2cb128eaf7684
Juanfrahh <carlostortosa2005@gmail.com>
2025-10-08 16:23:28 +0200

    Auto-commit

--- Desenvolupament WEB en entorn client/BotonFooter/js/script.js
+++ Desenvolupament WEB en entorn client/BotonFooter/js/script.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     if (footer.classList.contains('activo')) {
       boton.textContent = 'X Cerrar';
-      
+      //
     } else {
       boton.textContent = 'Descubre más...';
     }
