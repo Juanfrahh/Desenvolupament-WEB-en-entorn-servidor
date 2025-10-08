c541fbfe74d1f827289f47535df512022ffca5c6
Juanfrahh <carlostortosa2005@gmail.com>
2025-10-08 15:52:55 +0200

    Auto-commit

--- Desenvolupament WEB en entorn client/BotonFooter/js/script.js
+++ Desenvolupament WEB en entorn client/BotonFooter/js/script.js
@@ -9,7 +9,7 @@ btn.addEventListener('click', function() {
     // Cambiar el texto del botón
     if (footer.classList.contains('abierto')) {
         btn.textContent = 'XCerrar';
-        btn.bac = 'red';
+        btn. = 'red';
     } else {
         btn.textContent = 'Descubre más...';
     }
