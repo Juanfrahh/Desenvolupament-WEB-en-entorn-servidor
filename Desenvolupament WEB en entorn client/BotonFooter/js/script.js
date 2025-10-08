b7dfc10ed7548bd105d7b21a573b20093dc94d28
Juanfrahh <carlostortosa2005@gmail.com>
2025-10-08 15:26:18 +0200

    Auto-commit

--- Desenvolupament WEB en entorn client/BotonFooter/js/script.js
+++
@@ -1,8 +0,0 @@
-// Hazme que el id footer aparezca el footer al hacer click en el boton flotante
-
-document.querySelector('.btn-flotante').addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector('#footer').scrollIntoView({
-        behavior: 'smooth'
-    });
-});
