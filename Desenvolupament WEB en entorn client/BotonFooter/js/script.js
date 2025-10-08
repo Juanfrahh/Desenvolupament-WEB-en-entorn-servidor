dac0d03b48daf9fcfb40c6516aa34f233e802718
Juanfrahh <carlostortosa2005@gmail.com>
2025-10-08 15:58:31 +0200

    Auto-commit

--- Desenvolupament WEB en entorn client/BotonFooter/js/script.js
+++ Desenvolupament WEB en entorn client/BotonFooter/js/script.js
@@ -1,4 +1,4 @@
-const btn = document.qu('btn-footer');
+const btn = document.querySelector('btn-footer');
 const footer = document.getElementById('footer');
 
 btn.addEventListener('click', function() {
