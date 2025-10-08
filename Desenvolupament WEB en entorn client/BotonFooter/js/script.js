c348c78f55e9b505a99bcac9bd9ca0a70f0a0511
Juanfrahh <carlostortosa2005@gmail.com>
2025-10-08 15:48:55 +0200

    Auto-commit

--- Desenvolupament WEB en entorn client/BotonFooter/js/script.js
+++ Desenvolupament WEB en entorn client/BotonFooter/js/script.js
@@ -1,3 +1,5 @@
-//Al hacer click en el boton descubre mas quiero que se habra el footer de abajo y se cambie el nombre de Descubre más... a XCerrar.
+//Al hacer click en el boton descubre mas quiero que se habra el footer de abajo y se cambie el nombre de Descubre más... a XCerrar.//
+
+
 
 
