21da776c5106e07807b4f66bacaa1febb68eb74a
Juanfrahh <carlostortosa2005@gmail.com>
2025-10-18 12:48:50 +0200

    Auto-commit

--- Desenvolupament WEB en entorn client/Seguro/js.js
+++ Desenvolupament WEB en entorn client/Seguro/js.js
@@ -92,7 +92,7 @@ function validarFormulario(e) {
 
   // Validación: si falta un campo
   if (tipo === '' || anio === '' || !cobertura) {
-    mostrarError('<Falta un campo por rellenar');
+    mostrarError('<p>Falta un campo por rellenar');
     return;
   }
 
