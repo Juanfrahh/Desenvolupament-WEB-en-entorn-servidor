076458fc5829b1d5db14e5135edaac50378e9449
Juanfrahh <carlostortosa2005@gmail.com>
2025-10-20 10:48:27 +0200

    Auto-commit

--- Desenvolupament WEB en entorn client/Gastos/app.js
+++ Desenvolupament WEB en entorn client/Gastos/app.js
@@ -51,11 +51,6 @@ class UI {
 
     // Insertar en el DOM (antes del formulario)
     document.querySelector('.primario').insertBefore(div, document.querySelector('#agregar-gasto'));
-
-    // Quitar después de 3 segundos
-    setTimeout(() => {
-      if (div) div.remove();
-    }, 3000);
   }
 
   imprimirGastosListado(gastos) {
