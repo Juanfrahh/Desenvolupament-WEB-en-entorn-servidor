f44671f13c1ec1cf9ca3f0b21340ab37e7632c9a
Juanfrahh <carlostortosa2005@gmail.com>
2025-10-27 15:56:30 +0100

    Auto-commit

--- Desenvolupament WEB en entorn client/Carrito/index.js
+++ Desenvolupament WEB en entorn client/Carrito/index.js
@@ -11,7 +11,7 @@ let articulosCarrito = [];
 cargarEventListeners();
 
 function cargarEventListeners() {
-        listaCursos.addEventListener('click', agregarCurso);
+    listaCursos.addEventListener('click', agregarCurso);
 
     carrito.addEventListener('click', eliminarCurso);
 
