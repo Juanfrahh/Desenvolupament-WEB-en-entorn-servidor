254c93f51883b6318bcfd132d907c040e4e128d1
Juanfrahh <carlostortosa2005@gmail.com>
2025-11-03 16:45:05 +0100

    Auto-commit

--- Desenvolupament WEB en entorn client/Json/js/nuevocliente.js
+++ Desenvolupament WEB en entorn client/Json/js/nuevocliente.js
@@ -1,5 +1,5 @@
-// js/nuevocliente.js
-import { nuevoCliente } from './API.js';
+import { mostrarAlerta, validar } from './funciones.js';
+import { nuevoCliente } from './api.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   const formulario = document.querySelector('#formulario');
@@ -14,38 +14,14 @@ async function validarCliente(e) {
   const telefono = document.querySelector('#telefono').value.trim();
   const empresa = document.querySelector('#empresa').value.trim();
 
-  if (nombre === '' || email === '' || telefono === '' || empresa === '') {
+  const cliente = { nombre, email, telefono, empresa };
+
+  if (validar(cliente)) {
     mostrarAlerta('Todos los campos son obligatorios');
     return;
   }
 
-  const cliente = { nombre, email, telefono, empresa };
-
   await nuevoCliente(cliente);
-
   alert('Cliente agregado correctamente');
   window.location.href = 'index.html';
-}
-
-function mostrarAlerta(mensaje) {
-  const existe = document.querySelector('.bg-red-100');
-  if (existe) return;
-
-  const alerta = document.createElement('p');
-  alerta.classList.add(
-    'bg-red-100',
-    'border-red-400',
-    'text-red-700',
-    'px-4',
-    'py-3',
-    'rounded',
-    'max-w-lg',
-    'mx-auto',
-    'mt-6',
-    'text-center'
-  );
-  alerta.textContent = mensaje;
-  document.querySelector('#formulario').appendChild(alerta);
-
-  setTimeout(() => alerta.remove(), 3000);
 }
