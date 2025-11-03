18f2fa4154fce40c49ee74602a512649bd45e338
Juanfrahh <carlostortosa2005@gmail.com>
2025-11-03 16:40:17 +0100

    Auto-commit

--- Desenvolupament WEB en entorn client/Json/js/app.js
+++ Desenvolupament WEB en entorn client/Json/js/app.js
@@ -12,21 +12,14 @@ async function mostrarClientes() {
 
     const row = document.createElement('tr');
     row.innerHTML = `
-      <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-        <p class="text-sm leading-5 font-medium text-gray-700">${nombre}</p>
-      </td>
-      <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-        <p class="text-sm leading-5 text-gray-700">${telefono}</p>
-      </td>
-      <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-        <p class="text-sm leading-5 text-gray-700">${empresa}</p>
-      </td>
-      <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
+      <td class="px-6 py-4 border-b border-gray-200">${nombre}</td>
+      <td class="px-6 py-4 border-b border-gray-200">${telefono}</td>
+      <td class="px-6 py-4 border-b border-gray-200">${empresa}</td>
+      <td class="px-6 py-4 border-b border-gray-200">
         <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
         <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900 eliminar">Eliminar</a>
       </td>
     `;
-
     listado.appendChild(row);
   });
 
@@ -37,7 +30,6 @@ async function confirmarEliminar(e) {
   if (e.target.classList.contains('eliminar')) {
     const id = parseInt(e.target.dataset.cliente);
     const confirmar = confirm('¿Deseas eliminar este cliente?');
-
     if (confirmar) {
       await eliminarCliente(id);
       window.location.reload();
