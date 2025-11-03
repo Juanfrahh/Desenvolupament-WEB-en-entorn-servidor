a5689eea5948d8f17653eaf71fa5f11f87990880
Juanfrahh <carlostortosa2005@gmail.com>
2025-11-03 16:44:58 +0100

    Auto-commit

--- Desenvolupament WEB en entorn client/Json/js/funciones.js
+++ Desenvolupament WEB en entorn client/Json/js/funciones.js
@@ -1,1 +1,13 @@
-//funciones generales que pueden servir para varios ficheros
+// funciones.js
+export function mostrarAlerta(mensaje) {
+  const alerta = document.createElement('p');
+  alerta.textContent = mensaje;
+  alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'p-3', 'rounded', 'mt-4', 'text-center');
+  document.querySelector('#formulario').appendChild(alerta);
+
+  setTimeout(() => alerta.remove(), 3000);
+}
+
+export function validar(objeto) {
+  return !Object.values(objeto).every(input => input !== '');
+}
