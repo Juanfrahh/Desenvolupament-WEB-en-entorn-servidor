7d913f0701624b4c098d2fffd339579de047953a
Juanfrahh <carlostortosa2005@gmail.com>
2025-11-03 16:33:17 +0100

    Auto-commit

--- Desenvolupament WEB en entorn client/Json/js/API.js
+++ Desenvolupament WEB en entorn client/Json/js/API.js
@@ -1,1 +1,7 @@
 //Acciones con la API de Json Server
+const URL = 'http://localhost:4000/clientes';
+
+export async function obtenerClientes() {
+  const respuesta = await fetch(URL);
+  return await respuesta.json();
+}
