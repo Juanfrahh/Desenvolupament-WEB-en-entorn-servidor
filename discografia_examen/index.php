893791ee5f21a1b85b56668b9fb7b615d2299ab8
Juanfrahh <carlostortosa2005@gmail.com>
2025-11-08 19:57:55 +0100

    Auto-commit

--- discografia_examen/index.php
+++ discografia_examen/index.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8"> <!-- Codificación de caracteres -->
     <title>Document</title> <!-- Título de la página: cambiar según el tema -->
     <link rel="stylesheet" href="css/estilo.css"/> <!-- CSS de la página: opcional cambiar por otro estilo -->
-
     <?php
         include("datos.ini.php");     // Archivo con funciones o datos generales: cambiar por ejemplo a datosPeliculas.ini.php
         include("conexion.ini.php");  // Conexión a la base de datos: normalmente no se cambia
