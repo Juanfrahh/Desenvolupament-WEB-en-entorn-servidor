f11579dc00e43a44afce46db01682519447bb325
Juanfrahh <carlostortosa2005@gmail.com>
2025-11-08 20:18:58 +0100

    Auto-commit

--- discografia_examen/cancionnueva.php
+++ discografia_examen/cancionnueva.php
@@ -3,15 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <?php
-		include("datos.ini.php");
-        include("conexion.ini.php");
-        include("cancion.ini.php");
-	?>
-    <title>Document</title>
+        // Incluimos los archivos necesarios para usar las funciones y clases del proyecto
+        include("datos.ini.php");      // Contiene las funciones para mostrar formularios y listar discos/canciones
+        include("conexion.ini.php");   // Contiene la clase Conexion para conectarse a la base de datos
+        include("cancion.ini.php");    // Contiene la clase Cancion con sus métodos (como registrarCancion)
+    ?>
+    <title>Document</title> <!-- Título de la página -->
 </head>
 <body>
     <?php
-        $cancion = new Cancion($_GET['titulo'],$_GET['cod'],'','','');
+        // Creamos un objeto Cancion con los datos que vienen por GET
+        // $_GET['titulo'] -> el título del álbum o canción
+        // $_GET['cod'] -> código del álbum
+        // Los demás campos se dejan vacíos porque se completarán en el formulario
+        $cancion = new Cancion($_GET['titulo'], $_GET['cod'], '', '', '');
+
+        // Llamamos a la función que genera el formulario para registrar una nueva canción
+        // Esta función imprime el formulario HTML y, si se envía, registra la canción en la BD
         formularioCancion($cancion);
     ?>
 </body>
