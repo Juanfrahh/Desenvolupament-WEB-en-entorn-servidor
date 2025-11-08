a4863af386415bfb1215d05d514e5df2f9194f5d
Juanfrahh <carlostortosa2005@gmail.com>
2025-11-08 19:57:30 +0100

    Auto-commit

--- discografia_examen/index.php
+++ discografia_examen/index.php
@@ -1,18 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<title>Document</title>
-		<link rel="stylesheet" href="css/estilo.css"/>
-		<?php
-			include("datos.ini.php");
-			include("conexion.ini.php");
-			include("album.ini.php");
-		?>
-	</head>
-	<body>
-		<?php
-			datosDiscografia();
-		?>
-	</body>
+<head>
+    <meta charset="UTF-8"> <!-- Codificación de caracteres -->
+    <title>Document</title> <!-- Título de la página: cambiar según el tema -->
+    <link rel="stylesheet" href="css/estilo.css"/> <!-- CSS de la página: opcional cambiar por otro estilo -->
+
+    <?php
+        include("datos.ini.php");     // Archivo con funciones o datos generales: cambiar por ejemplo a datosPeliculas.ini.php
+        include("conexion.ini.php");  // Conexión a la base de datos: normalmente no se cambia
+        include("album.ini.php");     // Archivo con funciones específicas de la discografía: cambiar por ejemplo a peliculas.ini.php
+    ?>
+</head>
+<body>
+    <?php
+        datosDiscografia(); // Función que muestra la discografía: cambiar por ejemplo a datosPeliculas()
+    ?>
+</body>
 </html>
