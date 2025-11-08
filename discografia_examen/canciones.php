ce40f793eae066abfaf43a39c840f2f90dbca8e1
Juanfrahh <carlostortosa2005@gmail.com>
2025-11-08 20:19:35 +0100

    Auto-commit

--- discografia_examen/canciones.php
+++ discografia_examen/canciones.php
@@ -3,14 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <?php
-		include("datos.ini.php");
-        include("conexion.ini.php");
-        include("cancion.ini.php");
-	?>
-    <title>Document</title>
+        // Incluimos los archivos necesarios para usar funciones y clases del proyecto
+        include("datos.ini.php");      // Contiene funciones para formularios y listados de discos/canciones
+        include("conexion.ini.php");   // Contiene la clase Conexion para conectarse a la base de datos
+        include("cancion.ini.php");    // Contiene la clase Cancion con sus métodos
+    ?>
+    <title>Document</title> <!-- Título de la página -->
 </head>
 <body>
     <?php
+        // Llamamos a la función que genera el formulario de búsqueda de canciones
+        // Esta función imprime el formulario HTML y, si se envía, llama a datosBuscados() para mostrar resultados
         formularioBuscarCancion();
     ?>
 </body>
