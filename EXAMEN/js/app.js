814659403ff3938ddf7cfbf807da26eed878bc92
Juanfrahh <carlostortosa2005@gmail.com>
2025-11-12 17:38:29 +0100

    Auto-commit

--- EXAMEN/js/app.js
+++ EXAMEN/js/app.js
@@ -171,7 +171,7 @@ function iniciarApp() {
         btnCerrar.textContent = 'Cerrar';
         btnCerrar.setAttribute('data-bs-dismiss', 'modal');
 
-    //
+    // Archivo Favorito
         if (esFavoritos || esDesdeFavoritos) {
             const btnEliminar = document.createElement('button');
             btnEliminar.classList.add('btn', 'btn-danger', 'col');
