d75176ad35ec8c2c8221ca1c8016ffe24109f3ab
Juanfrahh <carlostortosa2005@gmail.com>
2025-11-12 15:31:59 +0100

    Auto-commit

--- EXAMEN/js/app.js
+++ EXAMEN/js/app.js
@@ -7,7 +7,7 @@ const CategoriaSelect = document.querySelector('#moneda'); // Select con la mone
 
 // Función que llena el select de criptomonedas
 function llenarSelectCriptos(categoria) {
-  categoria.forEach(cripto => {
+  categoria.forEach(categoria => {
     const { FullName, Name } = cripto.CoinInfo; // Obtenemos el nombre completo y el símbolo
 
     const option = document.createElement('option'); // Creamos un elemento option
