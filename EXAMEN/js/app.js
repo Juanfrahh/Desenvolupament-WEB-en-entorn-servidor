d2e04127d4e5fea1e6fbb9020b0141a7a19ac42c
Juanfrahh <carlostortosa2005@gmail.com>
2025-11-12 18:12:27 +0100

    Auto-commit

--- EXAMEN/js/app.js
+++ EXAMEN/js/app.js
@@ -112,7 +112,7 @@ function iniciarApp() {
             favoritos.forEach(receta => {
                 const { idMeal, strMeal, strMealThumb } = receta;
                 const divReceta = document.createElement('div');
-                recetaDiv.classList.add('col-md-4');
+                divReceta.classList.add('col-md-4');
 
                 recetaDiv.innerHTML = `
                     <div class="card mb-4">
