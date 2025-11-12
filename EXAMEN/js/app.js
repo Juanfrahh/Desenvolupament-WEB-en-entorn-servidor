7b79f8299a16e81266894f07bd3d6458655965c9
Juanfrahh <carlostortosa2005@gmail.com>
2025-11-12 15:46:31 +0100

    Auto-commit

--- EXAMEN/js/app.js
+++ EXAMEN/js/app.js
@@ -17,7 +17,7 @@ function llenarSelectCategorias(categoria) {
     const option = document.createElement('option'); // Creamos un elemento option
     option.value = Name; // Valor será el símbolo de la cripto
     option.textContent = FullName; // Texto visible será el nombre completo
-    CategoriaSelectSelect.appendChild(option); // Agregamos la opción al select
+    CategoriaSelect.appendChild(option); // Agregamos la opción al select
   });
 }
 // Función que maneja el submit del formulario
