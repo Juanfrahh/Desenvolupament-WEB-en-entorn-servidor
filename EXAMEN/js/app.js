7df1a5dc3111141c5ca1447940d510811ae68d7a
Juanfrahh <carlostortosa2005@gmail.com>
2025-11-12 17:32:09 +0100

    Auto-commit

--- EXAMEN/js/app.js
+++ EXAMEN/js/app.js
@@ -108,7 +108,6 @@ function iniciarApp() {
                 return;
             }
 
-            mensajeResultados.textContent = `Se encontraron ${recetas.length} recetas en la categoría "${categoria}".`;
 
             favoritos.forEach(receta => {
                 const { idMeal, strMeal, strMealThumb } = receta;
