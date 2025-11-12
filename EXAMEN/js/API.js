ed8386fd4280dae9381b58c5a36d7bfc0a67e8cc
Juanfrahh <carlostortosa2005@gmail.com>
2025-11-12 16:09:53 +0100

    Auto-commit

--- EXAMEN/js/API.js
+++ EXAMEN/js/API.js
@@ -24,7 +24,7 @@ export async function obtenerRecetas() {
    - Obtiene un único cliente según su ID.
    - Ideal para cuando se necesita editar o ver detalles.
 ===================================================== */
-export async function obtenerCategoria(id) {
+export async function obtenerrea(id) {
   // Se hace la petición GET al endpoint específico (clientes/ID)
   const respuesta = await fetch(`${URL}/${id}`);
   // Se devuelve el cliente encontrado en formato JSON
