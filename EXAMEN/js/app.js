6064c887463c29c06de8d5515a2f7e559ec73fec
Juanfrahh <carlostortosa2005@gmail.com>
2025-11-12 17:00:55 +0100

    Auto-commit

--- EXAMEN/js/app.js
+++ EXAMEN/js/app.js
@@ -145,4 +145,4 @@ function iniciarApp() {
 
 
 //creame dos botones en el model para cerra la ventana y otro para incluir la receta a Favoritos(localStorage) boton favoritos btn, btn dange, col y el boton cerrar btn btn-secondary col
-//si la receta ya esta en favoritos 
+//si la receta ya esta en favoritos el boton permitira 
