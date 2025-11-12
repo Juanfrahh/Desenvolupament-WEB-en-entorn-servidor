9f8e97293b16b89bcfcc25782744df289fbcc39e
Juanfrahh <carlostortosa2005@gmail.com>
2025-11-12 17:24:50 +0100

    Auto-commit

--- EXAMEN/js/app.js
+++ EXAMEN/js/app.js
@@ -199,7 +199,7 @@ function iniciarApp() {
                 btnFavorito.textContent = 'Eliminar de Favoritos';
             }
 
-            // actualizar lista
+            // Actualizar lista
             if (document.querySelector('main h2')?.textContent.includes('Favoritos')) {
                 mostrarFavoritos();
             }
