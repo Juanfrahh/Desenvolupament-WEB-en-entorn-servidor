fe40e9d5ab459f913907a43a5301fc11d0b27bc7
Juanfrahh <carlostortosa2005@gmail.com>
2025-11-12 15:24:43 +0100

    Auto-commit

--- EXAMEN/js/app.js
+++ EXAMEN/js/app.js
@@ -1,9 +1,4 @@
 //código js para prueba 1ª evaluación
-// Selección de elementos del DOM
-const RecetasSelect = document.querySelector('#criptomonedas'); // Select con las criptomonedas
-const monedaSelect = document.querySelector('#moneda'); // Select con la moneda fiat
-const formulario = document.querySelector('#formulario'); // Formulario donde se seleccionan las opciones
-const resultadoDiv = document.querySelector('#resultado'); // Div donde se mostrará la información de la cotización
 
 document.addEventListener('DOMContentLoaded', obtenerCategoriasRecetas);
 
@@ -22,14 +17,3 @@ async function obtenerCategoriasRecetas() {
   }
 }
 
-// Función que llena el select de criptomonedas
-function llenarSelectCriptos(criptos) {
-  criptos.forEach(cripto => {
-    const { FullName, Name } = cripto.CoinInfo; // Obtenemos el nombre completo y el símbolo
-
-    const option = document.createElement('option'); // Creamos un elemento option
-    option.value = Name; // Valor será el símbolo de la cripto
-    option.textContent = FullName; // Texto visible será el nombre completo
-    criptomonedasSelect.appendChild(option); // Agregamos la opción al select
-  });
-}
