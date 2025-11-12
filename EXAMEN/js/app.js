51b8cd1e08b0b047db16306f2e30961df7badace
Juanfrahh <carlostortosa2005@gmail.com>
2025-11-12 17:59:11 +0100

    Auto-commit

--- EXAMEN/js/app.js
+++ EXAMEN/js/app.js
@@ -108,7 +108,7 @@ function iniciarApp() {
                 return;
             }
 
-            contenedorResultado.textContent = ´Tienes ${}`
+            contenedorResultado.textContent = Tienes ${}`
 
             favoritos.forEach(receta => {
                 const { idMeal, strMeal, strMealThumb } = receta;
