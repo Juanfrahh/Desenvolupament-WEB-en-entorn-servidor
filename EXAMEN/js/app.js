aad09dd14d8685c7b37a437a8bc64927ff3a9700
Juanfrahh <carlostortosa2005@gmail.com>
2025-11-12 16:25:52 +0100

    Auto-commit

--- EXAMEN/js/app.js
+++ EXAMEN/js/app.js
@@ -74,4 +74,4 @@ function iniciarApp() {
     }
 }
 
-añadiremos un mensaje con el numero de resultados de las recetas segun la categoria 
+añadiremos un mensaje con el numero de resultados de las recetas segun la categoria si no hubiera resultado 
