860f76c735bfcf3affdba1eef19a243dc5ec742b
Juanfrahh <carlostortosa2005@gmail.com>
2025-11-12 15:28:44 +0100

    Auto-commit

--- EXAMEN/js/app.js
+++ EXAMEN/js/app.js
@@ -2,22 +2,7 @@
 const CategoriaSelect = document.querySelector('#moneda'); // Select con la moneda fiat
 
 
-document.addEventListener('DOMContentLoaded', obtenerCategoriasRecetas);
 
-async function obtenerCategoriasRecetas() {
-  const url = 'www.themealdb.com/api/json/v1/1/categories.php';
-  try {
-    const respuesta = await fetch(url); // Petición a la API
-    const data = await respuesta.json(); // Parseamos la respuesta JSON
-
-    const Recetas = data.Data; // Obtenemos el array de criptomonedas
-
-    llenarSelectCriptos(Recetas); // Llenamos el select con las opciones
-  } catch (error) {
-    mostrarError('Error al cargar las criptomonedas'); // Mostramos error si falla la petición
-    console.error(error);
-  }
-}
 
 // Función que maneja el submit del formulario
 function submitFormulario(e) {
