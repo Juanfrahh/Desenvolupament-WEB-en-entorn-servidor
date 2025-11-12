cccef36fe5311bfde75b77ccc9c1b8a53ec7b717
Juanfrahh <carlostortosa2005@gmail.com>
2025-11-12 15:22:37 +0100

    Auto-commit

--- EXAMEN/js/app.js
+++ EXAMEN/js/app.js
@@ -10,7 +10,7 @@ async function obtenerCategoriasRecetas() {
 
     const Recetas = data.Data; // Obtenemos el array de criptomonedas
 
-    llenarSelectCriptos(criptos); // Llenamos el select con las opciones
+    llenarSelectCriptos(rec); // Llenamos el select con las opciones
   } catch (error) {
     mostrarError('Error al cargar las criptomonedas'); // Mostramos error si falla la petición
     console.error(error);
