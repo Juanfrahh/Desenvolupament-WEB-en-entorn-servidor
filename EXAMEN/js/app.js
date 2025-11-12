0a9df49e0345c030d4b41742614f79fb0c5aa9bb
Juanfrahh <carlostortosa2005@gmail.com>
2025-11-12 15:37:21 +0100

    Auto-commit

--- EXAMEN/js/app.js
+++ EXAMEN/js/app.js
@@ -22,7 +22,6 @@ function submitFormulario(e) {
   e.preventDefault(); // Prevenimos el comportamiento por defecto (recargar la página)
 
   const moneda = CategoriaSelect.value; // Obtenemos la moneda seleccionada
-  const cripto = criptomonedasSelect.value; // Obtenemos la cripto seleccionada
 
   if (moneda === '' || cripto === '') {
     mostrarError('Debes seleccionar ambas opciones'); // Validamos que se seleccione todo
