b0a48428feea6fa954afc27138a7a46ab7d79d24
Juanfrahh <carlostortosa2005@gmail.com>
2025-11-14 15:57:42 +0100

    Auto-commit

--- tareas/header.php
+++ tareas/header.php
@@ -10,7 +10,7 @@ require_once __DIR__ . '/../config/config.php';
 <body>
 <header>
     <nav>
-        <a href="../public/index.php">Inicio</a> |
+        <a href="index.php">Inicio</a> |
         <a href="../public/add_tarea.php">Añadir Tarea</a> |
         <a href="../public/search.php">Buscar Tareas</a> |
         <?php if(estaAutenticado()): ?>
