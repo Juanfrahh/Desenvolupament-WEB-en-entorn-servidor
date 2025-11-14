76641f0923ef4b3130ea210e628b3ce6985e52ac
Juanfrahh <carlostortosa2005@gmail.com>
2025-11-14 15:45:52 +0100

    Auto-commit

--- tareas/index.php
+++ tareas/index.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['usuario'])) {
 }
 
 // Conexión a la base de datos
-include 'conexion.ini.php'; // ✅ Incluimos la clase de conexión
+include 'conexionphp'; // ✅ Incluimos la clase de conexión
 
 // ✅ Creamos la conexión usando la clase
 $conectar = new Conexion('localhost', 'root', '', 'tareas');
