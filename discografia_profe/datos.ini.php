646473b61eb202d3ded6637a3a7c64d69c07b422
Juanfrahh <carlostortosa2005@gmail.com>
2025-11-14 12:29:04 +0100

    Auto-commit

--- discografia_profe/datos.ini.php
+++ discografia_profe/datos.ini.php
@@ -21,7 +21,7 @@
 		echo '</form>';
 		
 		if(isset($_POST["titulo"])){
-			$conectar = new Conexion('localhost','user','user','discografia');
+			$conectar = new Conexion('localhost','root','user','discografia');
 			$conexion = $conectar->conectionPDO();
 			$album = new Album('',$_POST['titulo'],$_POST['discografia'],$_POST['formato'],$_POST['fechaLanzamiento'],$_POST['fechaCompra'],$_POST['precio']);
 
