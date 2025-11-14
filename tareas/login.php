46e539f1035311cbf69f717a0ef9feacfb912f9a
Juanfrahh <carlostortosa2005@gmail.com>
2025-11-14 15:51:26 +0100

    Auto-commit

--- tareas/login.php
+++ tareas/login.php
@@ -44,7 +44,7 @@ if (isset($_POST['accion']) && $_POST['accion'] === 'login') {
     <?php echo $mensaje; ?>
     <form action="login.php" method="post">
         <input type="text" name="usuario" placeholder="Usuario" required><br>
-        <input type="email" name="email" place>
+        <input type="email" name="email" placeholder>
         <input type="password" name="password" placeholder="Contraseña" required><br>
         <input type="hidden" name="accion" value="login">
         <input type="submit" value="Entrar">
