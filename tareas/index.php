e735ef7cfc171603c74830ca3848cee8823e6cb7
Juanfrahh <carlostortosa2005@gmail.com>
2025-11-14 16:03:55 +0100

    Auto-commit

--- tareas/index.php
+++ tareas/index.php
@@ -34,7 +34,7 @@ include 'header.php';
             <td>
                 <?php if(!$t['completada']): ?>
                     <a href="edittarea.php?id=<?= $t['id'] ?>">Editar</a> |
-                    <a href="delete_tarea.php?id=<?= $t['id'] ?>" onclick="return confirm('¿Eliminar tarea?')">Eliminar</a>
+                    <a href="deletetarea.php?id=<?= $t['id'] ?>" onclick="return confirm('¿Eliminar tarea?')">Eliminar</a>
                 <?php else: ?>
                     -
                 <?php endif; ?>
