32025fe13aef3ab19e8347b8d66dc701838e5bd7
Juanfrahh <carlostortosa2005@gmail.com>
2025-11-14 15:45:04 +0100

    Auto-commit

--- tareas/index.php
+++ tareas/index.php
@@ -48,7 +48,6 @@ $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
     <p>
         <a href="perfil.php">Mi perfil</a> | 
         <a href="logout.php">Cerrar sesión</a>
-        <a href="album.ini.php">Album</a>
     </p>
 
     <hr>
