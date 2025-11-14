7ed387e20fc013b76b27a749a9539f1686d783f5
Juanfrahh <carlostortosa2005@gmail.com>
2025-11-14 16:26:07 +0100

    Auto-commit

--- tareas/Conexion.php
+++ tareas/Conexion.php
@@ -1,39 +1,35 @@
 <?php
-class Conexion {
+// Conexion.php
+require_once __DIR__ . '/config.php';
 
-    private $ip = "localhost";
-    private $nombre = "usr_tareas";
-    private $password = "usr_tareas";
-    private $bd = "tareas";
+class Conexion {
+    private $host = DB_HOST;
+    private $db = DB_NAME;
+    private $user = DB_USER;
+    private $pass = DB_PASS;
     private $pdo;
 
     public function __construct() {
         $this->conectarPDO();
     }
 
-    // Conexión con PDO
     private function conectarPDO() {
-        $opciones = array(
+        $opciones = [
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
-        );
-
+        ];
         try {
             $this->pdo = new PDO(
-                "mysql:host={$this->ip};dbname={$this->bd}",
-                $this->nombre,
-                $this->password,
+                "mysql:host={$this->host};dbname={$this->db};charset=utf8",
+                $this->user,
+                $this->pass,
                 $opciones
             );
-
-            // Lanzar excepciones en caso de error
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
         } catch (PDOException $e) {
             die("Error conectando a la base de datos: " . $e->getMessage());
         }
     }
 
-    // Getter del objeto PDO
     public function getConexion() {
         return $this->pdo;
     }
