f1506afcd6655296a28a37c1cf77858590b441f9
Juanfrahh <carlostortosa2005@gmail.com>
2025-11-14 17:54:23 +0100

    Auto-commit

--- tareas/Tarea.php
+++ tareas/Tarea.php
@@ -68,4 +68,21 @@ class Tarea {
     }
 
     public function ultimasCincoAcciones() {
-        $
+        $sql = "SELECT t.*,
+                       u1.nombre AS creador,
+                       u2.nombre AS modificador,
+                       u3.nombre AS completador,
+                       GREATEST(COALESCE(UNIX_TIMESTAMP(fecha_creacion),0),
+                                COALESCE(UNIX_TIMESTAMP(fecha_modificacion),0),
+                                COALESCE(UNIX_TIMESTAMP(fecha_finalizacion),0)) AS ultima_accion_ts
+                FROM tareas t
+                LEFT JOIN usuarios u1 ON t.id_usr_crea = u1.id
+                LEFT JOIN usuarios u2 ON t.id_usr_mod = u2.id
+                LEFT JOIN usuarios u3 ON t.id_usr_comp = u3.id
+                ORDER BY ultima_accion_ts DESC
+                LIMIT 5";
+        $stmt = $this->db->query($sql);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+?>
