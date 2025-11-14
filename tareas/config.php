208852430d604088ab3fd6de671de230f2a5a79f
Juanfrahh <carlostortosa2005@gmail.com>
2025-11-14 16:25:55 +0100

    Auto-commit

--- tareas/config.php
+++ tareas/config.php
@@ -2,11 +2,13 @@
 // config.php
 session_start();
 
+// Configuración de la BD (ajusta si fuera necesario)
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'tareas');
 define('DB_USER', 'usr_tareas');
 define('DB_PASS', 'usr_tareas');
 
+// Funciones auxiliares
 function estaAutenticado() {
     return isset($_SESSION['usuario_id']);
 }
@@ -20,5 +22,19 @@ function protegerPagina() {
 
 function limpiarEntrada($data) {
     return htmlspecialchars(strip_tags(trim($data)));
+}
+
+// Función para mostrar mensajes flash
+function flash($key, $value = null) {
+    if ($value === null) {
+        if (isset($_SESSION['flash'][$key])) {
+            $v = $_SESSION['flash'][$key];
+            unset($_SESSION['flash'][$key]);
+            return $v;
+        }
+        return null;
+    } else {
+        $_SESSION['flash'][$key] = $value;
+    }
 }
 ?>
