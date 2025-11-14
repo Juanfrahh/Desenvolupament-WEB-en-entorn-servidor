70fe59f94a331e32baf4bc08f4f0fba8e349c104
Juanfrahh <carlostortosa2005@gmail.com>
2025-11-14 15:58:03 +0100

    Auto-commit

--- tareas/header.php
+++ tareas/header.php
@@ -17,7 +17,7 @@ require_once __DIR__ . '/../config/config.php';
             <span>Hola, <?= $_SESSION['usuario_nombre'] ?></span>
             <img src="img/<?= $_SESSION['usuario_img'] ?>" alt="Perfil" width="30">
             <a href="perfil.php">Perfil</a> |
-            <a href="../public/logout.php">Cerrar sesión</a>
+            <a href="logout.php">Cerrar sesión</a>
         <?php else: ?>
             <a href="../public/login.php">Login</a> |
             <a href="../public/register.php">Registro</a>
