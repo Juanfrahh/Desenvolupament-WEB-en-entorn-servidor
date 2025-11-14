de1c336142c94053b5644dd8e4b700d50171c320
Juanfrahh <carlostortosa2005@gmail.com>
2025-11-14 12:18:43 +0100

    Auto-commit

--- discografia_profe/index.php
+++ discografia_profe/index.php
@@ -1,18 +1,38 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<title>Document</title>
-		<link rel="stylesheet" href="css/estilo.css"/>
-		<?php
-			include("datos.ini.php");
-			include("conexion.ini.php");
-			include("album.ini.php");
-		?>
-	</head>
-	<body>
-		<?php
-			datosDiscografia();
-		?>
-	</body>
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="css/estilo.css"/>
+	<?php
+		include("datos.ini.php");
+		include("conexion.ini.php");
+		include("album.ini.php");
+	?>
+</head>
+
+<body>
+
+	<!-- MENÚ SUPERIOR / LOGIN / PERFIL / LOGOUT -->
+	<div style="padding: 10px; background: #eee; border-bottom: 1px solid #ccc;">
+		<?php if (!isset($_SESSION['usuario_id'])): ?>
+
+			<a href="login.php">Iniciar sesión</a>
+
+		<?php else: ?>
+
+			<span>Bienvenido, <strong><?= $_SESSION['usuario'] ?></strong></span> |
+			<a href="perfil.php">Mi perfil</a> |
+			<a href="logout.php">Cerrar sesión</a>
+
+		<?php endif; ?>
+	</div>
+
+	<!-- CONTENIDO PRINCIPAL -->
+	<?php datosDiscografia(); ?>
+
+</body>
 </html>
