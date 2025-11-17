e80e5aab807016053e9bf3781a239029d7d97453
Juanfrahh <carlostortosa2005@gmail.com>
2025-11-17 17:17:00 +0100

    Auto-commit

--- Desenvolupament WEB en entorn client/Trimestre2/tiendaGuitarra/tiendaGuitarra/src/App.jsx
+++ Desenvolupament WEB en entorn client/Trimestre2/tiendaGuitarra/tiendaGuitarra/src/App.jsx
@@ -4,7 +4,7 @@ import Header from "./componentes/Header"
 function App() {
   return (
     <>
-      
+      <
           <header class="py-5 header">
         <div class="container-xl">
             <div class="row justify-content-center justify-content-md-between">
